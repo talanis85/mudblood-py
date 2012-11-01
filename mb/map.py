19cf8cde8a789ce86c55563c6b0e6e7e9f22a82e
Philip Kranz <philip.kranz@googlemail.com>
2012-11-01 14:10:12 +0100

    Hardcoded map language to german for now. Added a load function for old map format.

--- mb/map.py
+++ mb/map.py
@@ -1,14 +1,20 @@
 import json
+import re
+
+# EXCEPTIONS
+
+class InvalidMapFile(Exception):
+    pass
 
 directions = {
         'n': ('s', 0, -1, 0),
-        'ne': ('sw', 1, -1, 0),
-        'e': ('w', 1, 0, 0),
-        'se': ('nw', 1, 1, 0),
-        's': ('n', 0, -1, 0),
-        'sw': ('ne', -1, 1, 0),
-        'w': ('e', -1, 0, 0),
-        'nw': ('se', -1, -1, 0),
+        'no': ('sw', 1, -1, 0),
+        'o': ('w', 1, 0, 0),
+        'so': ('nw', 1, 1, 0),
+        's': ('n', 0, 1, 0),
+        'sw': ('no', -1, 1, 0),
+        'w': ('o', -1, 0, 0),
+        'nw': ('so', -1, -1, 0),
 #        'u': ('d', 0, 0, 1),
 #        'd': ('u', 0, 0, -1),
         }
@@ -52,6 +58,7 @@ class Room(object):
 class Edge(object):
     def __init__(self, dest):
         self.dest = dest
+        self.split = False
 
     @classmethod
     def fromJsonObj(cls, ob):
@@ -101,6 +108,48 @@ class Map(object):
             for e in self.rooms[r].edges:
                 self.rooms[r].edges[e].dest = self.rooms[self.rooms[r].edges[e].dest]
 
+    def load_old(self, f):
+        state = 0
+        line = 0
+        for l in f:
+            line += 1
+            l = l.strip()
+            if state == 0:
+                if l == "mudblood v1.0":
+                    state = 1
+                else:
+                    raise InvalidMapFile("Expected 'mudblood v1.0' in line {}".format(line))
+            elif state == 1:
+                nrooms = int(l)
+                for i in range(nrooms):
+                    self.rooms[i] = Room(i)
+                state = 2
+            elif state == 2:
+                if l == "":
+                    state = 3
+                else:
+                    m = re.match(r"^(\d+) (.+)$", l)
+                    if m is None:
+                        raise InvalidMapFile("Expected Room tag in line {}".format(line))
+                    self.rooms[int(m.group(1))].tag = m.group(2)
+            elif state == 3:
+                if l == "":
+                    state = 4
+                else:
+                    m = re.match(r"^(\d+)\|([^\|]*)\|(\d+)\|([^\|]*)\|(\d)(\d)(\d)$", l)
+                    if m is None:
+                        raise InvalidMapFile("Expected edge in line {}".format(line))
+                    if m.group(2) != "":
+                        e = Edge(self.rooms[int(m.group(3))])
+                        e.split = (m.group(5) == "1")
+                        self.rooms[int(m.group(1))].edges[m.group(2)] = e
+                    if m.group(4) != "":
+                        e = Edge(self.rooms[int(m.group(1))])
+                        e.split = (m.group(5) == "1")
+                        self.rooms[int(m.group(3))].edges[m.group(4)] = e
+            else:
+                pass
+
     def save(self, f):
         j = {"rooms": []}
 
@@ -113,6 +162,8 @@ class Map(object):
         self.rooms[self._nextRid] = Room(self._nextRid)
         self._nextRid += 1
 
+    def goto(self, id):
+        self.currentRoom = id
 
 class MapRenderer(object):
     def __init__(self, map):
@@ -124,9 +175,9 @@ class MapRenderer(object):
 class AsciiMapRenderer(MapRenderer):
     directionSymbols = {
             'n': '|',
-            'ne': '/',
-            'e': '-',
-            'se': '\\',
+            'no': '/',
+            'o': '-',
+            'so': '\\',
             's': '|',
             'sw': '/',
             'w': '-',
@@ -147,7 +198,7 @@ class AsciiMapRenderer(MapRenderer):
 
         self.visited.add(r.id)
 
-        if x >= 0 and x < w and y >= 0 and y < w:
+        if x >= 0 and x < w and y >= 0 and y < h:
             if r.id == self.map.currentRoom:
                 self.out[y*w+x] = ord('X')
             else:
@@ -157,15 +208,21 @@ class AsciiMapRenderer(MapRenderer):
             if d in directions:
                 newx = x
                 newy = y
-                for i in range(2):
+                if e.split:
                     newx += directions[d][1]
                     newy += directions[d][2]
-                    if newx >= 0 and newx < w and newy >= 0 and newy < w:
+                    if newx >= 0 and newx < w and newy >= 0 and newy < h:
                         self.out[newy*w+newx] = ord(self.directionSymbols[d])
-                newx += directions[d][1]
-                newy += directions[d][2]
+                else:
+                    for i in range(2):
+                        newx += directions[d][1]
+                        newy += directions[d][2]
+                        if newx >= 0 and newx < w and newy >= 0 and newy < h:
+                            self.out[newy*w+newx] = ord(self.directionSymbols[d])
+                    newx += directions[d][1]
+                    newy += directions[d][2]
 
-                self.renderRoom(e.dest, newx, newy, w, h)
+                    self.renderRoom(e.dest, newx, newy, w, h)
 
 if __name__ == "__main__":
     m = Map()
