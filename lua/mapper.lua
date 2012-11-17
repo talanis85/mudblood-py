59fe7344c282ca6d532348a63fcf29177a7894be
Philip Kranz <philip.kranz@googlemail.com>
2012-11-17 16:29:06 +0100

    mapper.lua: P() and V() make sense only when we are currently walking.

--- lua/mapper.lua
+++ lua/mapper.lua
@@ -43,20 +43,22 @@ function M.stop()
 end
 
 function M.P()
-    print("mapper.P()")
-    walk_semaphore = walk_semaphore + 1
+    if walker then
+        walk_semaphore = walk_semaphore + 1
+    end
 end
 
 function M.V()
-    print("mapper.V()")
-    if walk_semaphore > 0 then
-        walk_semaphore = walk_semaphore - 1
-    end
+    if walker then
+        if walk_semaphore > 0 then
+            walk_semaphore = walk_semaphore - 1
+        end
 
-    if walk_semaphore == 0 then
-        local status, err = coroutine.resume(walker)
-        if status ~= true then
-            error(err)
+        if walk_semaphore == 0 then
+            local status, err = coroutine.resume(walker)
+            if status ~= true then
+                error(err)
+            end
         end
     end
 end
