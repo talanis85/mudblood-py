80c84ce4d6a2d2801905567486c319963f777bbd
Philip Kranz <philip.kranz@googlemail.com>
2013-01-03 02:54:47 +0100

    Added py2exe support.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distribute_setup import use_setuptools
 use_setuptools()
 
+py2exe = None
+
+try:
+    import py2exe as p2e
+    py2exe = p2e
+except:
+    pass
+
 from setuptools import setup
 
 setup(
@@ -16,6 +24,8 @@ setup(
         packages = ['mudblood', 'mudblood.screen'],
         #scripts = ['bin/mudblood'],
         package_data = {'mudblood': ['lua/*.lua']},
+
+        console = ['mudblood/main.py'],
 
         entry_points = {
             'console_scripts': ['mudblood = mudblood.main:main']
