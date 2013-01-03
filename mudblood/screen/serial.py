a9ad6cb4ec7542cd423f97cf8c6a4a0477b48f47
Philip Kranz <philip.kranz@googlemail.com>
2013-01-03 13:19:43 +0100

    serial: Cleanup

--- mudblood/screen/serial.py
+++ mudblood/screen/serial.py
@@ -6,8 +6,6 @@ from mudblood import keys
 from mudblood import modes
 from mudblood import ansi
 
-from mudblood.main import MB
-
 class SerialSource(event.AsyncSource):
     def __init__(self):
         super(SerialSource, self).__init__()
@@ -44,7 +42,6 @@ class SerialScreen(screen.Screen):
                 pass
             elif isinstance(ev, screen.DestroyScreenEvent):
                 self.doneEvent()
-                print("Goodbye")
                 break
             elif isinstance(ev, screen.ModeScreenEvent):
                 self.mode = ev.mode
@@ -82,105 +79,3 @@ class SerialScreen(screen.Screen):
         sys.stdout.flush()
 
         self.nlines = len(lines)
-
-
-#class NormalMode(modes.BufferMode):
-#    def __init__(self):
-#        super(NormalMode, self).__init__()
-#
-#    def onKey(self, key):
-#        bindret = MB().session.bindings.key(key)
-#
-#        if bindret == True:
-#            pass
-#        elif bindret == False:
-#            MB().session.bindings.reset()
-#            if key == ord("\n"):
-#                self.addHistory()
-#
-#                MB().drain.put(event.InputEvent(self.getBuffer(), display=False))
-#                self.clearBuffer()
-#            elif key == keys.KEY_CTRL_BACKSLASH:
-#                MB().drain.put(event.ModeEvent("lua"))
-#            elif key == keys.KEY_ESC:
-#                MB().drain.put(event.ModeEvent("console"))
-#            elif key == keys.KEY_PGUP:
-#                MB().session.windows[0].scroll += 20
-#            elif key == keys.KEY_PGDN:
-#                MB().session.windows[0].scroll -= 20
-#                if MB().session.windows[0].scroll < 0:
-#                    MB().session.windows[0].scroll = 0
-#            else:
-#                super(NormalMode, self).onKey(key)
-#        else:
-#            MB().session.bindings.reset()
-#            if callable(bindret):
-#                MB().drain.put(event.CallableEvent(bindret))
-#            elif isinstance(bindret, basestring):
-#                MB().drain.put(event.InputEvent(bindret))
-#            else:
-#                MB().drain.put(event.LogEvent("Invalid binding.", "err"))
-#
-#normalMode = NormalMode()
-#
-#class ConsoleMode(modes.Mode):
-#    def onKey(self, key):
-#        if key == keys.KEY_ESC:
-#            MB().drain.put(event.ModeEvent("normal"))
-#        else:
-#            super(ConsoleMode, self).onKey(key)
-#
-#consoleMode = ConsoleMode()
-#
-#class LuaMode(modes.BufferMode):
-#    def onKey(self, key):
-#        if key == keys.KEY_ESC:
-#            MB().drain.put(event.ModeEvent("normal"))
-#        elif key == ord("\r"):
-#            self.addHistory()
-#
-#            MB().session.luaEval(self.getBuffer() + "\n")
-#            self.clearBuffer()
-#            MB().drain.put(event.ModeEvent("normal"))
-#        else:
-#            super(LuaMode, self).onKey(key)
-#
-#luaMode = LuaMode()
-#
-#class MapMode(modes.Mode):
-#    def onKey(self, key):
-#        if key == keys.KEY_ESC:
-#            MB().drain.put(event.ModeEvent("normal"))
-#        else:
-#            super(MapMode, self).onKey(key)
-#
-#mapMode = MapMode()
-#
-#class PromptMode(modes.BufferMode):
-#    def __init__(self):
-#        super(PromptMode, self).__init__()
-#        self.call = None
-#        self.text = ""
-#
-#    def getText(self):
-#        return self.text
-#
-#    def getCall(self):
-#        return self.call
-#
-#    def onEnter(self, text="", call=None):
-#        self.call = call
-#        self.text = text
-#
-#    def onKey(self, key):
-#        if key == keys.KEY_ESC:
-#            MB().drain.put(event.ModeEvent("normal"))
-#        elif key == ord("\r"):
-#            MB().drain.put(event.ModeEvent("normal"))
-#            if self.call:
-#                MB().drain.put(event.CallableEvent(self.call, self.getBuffer()))
-#            self.clearBuffer()
-#        else:
-#            super(PromptMode, self).onKey(key)
-#
-#promptMode = PromptMode()
