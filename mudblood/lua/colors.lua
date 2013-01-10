6ff08fe22446282ed4eeeb734f631a8099b34cf4
Philip Kranz <philip.kranz@googlemail.com>
2013-01-10 15:35:28 +0100

    lua/colors: Added remaining colors.

--- mudblood/lua/colors.lua
+++ mudblood/lua/colors.lua
@@ -3,8 +3,13 @@ local M = {}
 local esc = "\27"
 
 M.Off = esc .. "[0m"
+M.Black = esc .. "[30m"
 M.Red = esc .. "[31m"
 M.Green = esc .. "[32m"
+M.Yellow = esc .. "[33m"
 M.Blue = esc .. "[34m"
+M.Magenta = esc .. "[35m"
+M.Cyan = esc .. "[36m"
+M.White = esc .. "[37m"
 
 return M
