fc514c93a901223f20be48efb7e639a8f0bcf705
Philip Kranz <philip.kranz@googlemail.com>
2013-02-20 13:55:53 +0100

    screen/pgscreen: Support for windows and multiple columns.

--- mudblood/screen/pgscreen.py
+++ mudblood/screen/pgscreen.py
@@ -2,6 +2,7 @@ import threading
 
 import pygame
 import pygame.locals
+import pygame.gfxdraw
 
 from mudblood import event
 from mudblood import screen
@@ -50,18 +51,6 @@ keymap = {
         pygame.K_PAGEDOWN:      keys.KEY_PGDN,
 }
 
-colormap = {
-        colors.BLACK:           (0, 0, 0),
-        colors.RED:             (255, 100, 100),
-        colors.GREEN:           (0, 255, 0),
-        colors.YELLOW:          (255, 255, 0),
-        colors.BLUE:            (0, 0, 255),
-        colors.MAGENTA:         (255, 0, 255),
-        colors.CYAN:            (0, 255, 255),
-        colors.WHITE:           (255, 255, 255),
-        colors.DEFAULT:         (255, 255, 255),
-}
-
 def createScreen(master):
     return PygameScreen(master)
 
@@ -97,6 +86,15 @@ class Lua_Screen(lua.LuaExposedObject):
         else:
             self._screen.window_sizes[name] = value
 
+    def setColumns(self, num):
+        self._screen.columns = num
+    
+    def configFgColor(self, colornum, rgb):
+        self._screen.colormap_fg[colornum] = (rgb[1], rgb[2], rgb[3])
+
+    def configBgColor(self, colornum, rgb):
+        self._screen.colormap_bg[colornum] = (rgb[1], rgb[2], rgb[3])
+
     def scroll(self, value, name='main'):
         self._screen.moveScroll(name, value)
 
@@ -112,6 +110,31 @@ class PygameScreen(modalscreen.ModalScreen):
 
         self.windows = []
         self.window_sizes = {}
+        self.columns = 1
+
+        self.colormap_fg = {
+                colors.BLACK:           (0, 0, 0),
+                colors.RED:             (255, 100, 100),
+                colors.GREEN:           (0, 255, 0),
+                colors.YELLOW:          (255, 255, 0),
+                colors.BLUE:            (0, 0, 255),
+                colors.MAGENTA:         (255, 0, 255),
+                colors.CYAN:            (0, 255, 255),
+                colors.WHITE:           (255, 255, 255),
+                colors.DEFAULT:         (255, 255, 255),
+        }
+
+        self.colormap_bg = {
+                colors.BLACK:           (0, 0, 0),
+                colors.RED:             (255, 100, 100),
+                colors.GREEN:           (0, 255, 0),
+                colors.YELLOW:          (255, 255, 0),
+                colors.BLUE:            (0, 0, 255),
+                colors.MAGENTA:         (255, 0, 255),
+                colors.CYAN:            (0, 255, 255),
+                colors.WHITE:           (255, 255, 255),
+                colors.DEFAULT:         (0, 0, 0),
+        }
 
     def tick(self):
         pass
@@ -135,8 +158,6 @@ class PygameScreen(modalscreen.ModalScreen):
         # Init pygame
         pygame.init()
 
-        self.background = (0, 0, 0)
-
         # Init pygame screen
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
         pygame.display.set_caption('Mudblood')
@@ -149,6 +170,8 @@ class PygameScreen(modalscreen.ModalScreen):
         self.font = pygame.font.SysFont(self.fontname, self.fontsize)
         self.fontwidth, self.fontheight = self.font.size("a")
         self.antialias = False
+
+        pygame.key.set_repeat(500, 50)
 
         while True:
             ev = pygame.event.wait()
@@ -178,46 +201,150 @@ class PygameScreen(modalscreen.ModalScreen):
 
             self.doUpdate()
 
+    def drawWindow(self, wn, w, h, fixh=5):
+        surf = pygame.Surface((w, h))
+        surf.fill(self.colormap_bg[colors.DEFAULT])
+
+        background = self.colormap_bg[colors.DEFAULT]
+
+        y = 0
+        x = 0
+
+        gw = w / self.fontwidth
+        gh = h / self.fontheight
+
+        if wn == 'map':
+            # TODO
+            pass
+        else:
+            if wn in self.master.session.linebuffers:
+                lines = None
+                fixlines = None
+                scroll = self.getScroll(wn)
+                lb = self.master.session.linebuffers[wn]
+
+                if scroll > 0:
+                    pygame.gfxdraw.hline(surf, 0, w, h - fixh * self.fontheight - self.fontheight / 2,
+                                         self.colormap_fg[colors.WHITE])
+
+                    if wn == 'main':
+                        lines = lb.render(gw, scroll, gh - fixh - 1) \
+                              + [[]] + lb.render(gw, 0, fixh - 1) + [self.master.session.getPromptLine()]
+                    else:
+                        lines = lb.render(gw, scroll, gh - fixh - 1) \
+                              + [[]] + lb.render(gw, 0, fixh)
+                else:
+                    if wn == 'main':
+                        lines = lb.render(gw, scroll, gh - 1) \
+                              + [self.master.session.getPromptLine()]
+                    else:
+                        lines = lb.render(gw, scroll, gh)
+
+                if len(lines) < gh:
+                    y += (gh - len(lines)) * self.fontheight
+
+                for l in lines:
+                    x = 0
+                    for ch in l:
+                        char = self.font.render(ch[1], self.antialias,
+                                                self.colormap_fg[ch[0][0]],
+                                                (ch[0][1] == colors.DEFAULT and background or self.colormap_bg[ch[0][1]]))
+                        surf.blit(char, (x, y))
+                        x += self.fontwidth
+                    y += self.fontheight
+
+                if wn == 'main':
+                    y -= self.fontheight
+
+                    curline = self.font.render(self.normalMode.getBuffer(), self.antialias,
+                                               self.colormap_fg[colors.YELLOW], background)
+                    surf.blit(curline, (x, y))
+
+                    cursor = self.font.render("_", self.antialias, self.colormap_fg[colors.YELLOW])
+                    surf.blit(cursor, (x + self.normalMode.getCursor()*self.fontwidth, y))
+
+        return surf
+
     def doUpdate(self):
-        self.screen.fill(self.background)
+        background = self.colormap_bg[colors.DEFAULT]
+        self.screen.fill(background)
 
         border = 10
 
-        wh = (self.height - 2*border) / self.fontheight
-        ww = (self.width - 2*border) / self.fontwidth
+        gw = self.width / self.fontwidth
+        gh = self.height / self.fontheight - 3
 
-        lines = self.master.session.linebuffers['main'].render(ww, self.getScroll('main'), wh-2) \
-              + [self.master.session.getPromptLine()]
+        gww = gw / self.columns
+        ww = self.width / self.columns
 
-        x = border
-        y = border
-        for l in lines:
+        # Arrange windows
+        gx = 0
+        cc = self.columns - 1
+        cr = 0
+
+        layout = []
+        for c in range(self.columns):
+            layout.append([])
+
+        for w in self.windows + ['main']:
+            if w == 'main':
+                if cc > 0:
+                    if cr > 0:
+                        layout[cc][cr-1] = (layout[cc][cr-1][0], layout[cc][cr-1][1], gh - layout[cc][cr-1][1])
+                    cc = 0
+                    cr = 0
+                    gx = 0
+                wh = gh - gx
+            else:
+                wh = self.window_sizes[w]
+
+            if gx + wh > gh and cc > 0:
+                if cr > 0:
+                    layout[cc][cr-1] = (layout[cc][cr-1][0], layout[cc][cr-1][1], gh - layout[cc][cr-1][1])
+                cc -= 1
+                cr = 0
+                gx = 0
+
+            if gx + wh > gh:
+                wh = gh - gx
+
+            layout[cc].append((w, gx, wh))
+            cr += 1
+            gx += wh
+
+        # Draw column separators
+        for i in range(self.columns-1):
+            pygame.gfxdraw.vline(self.screen, ww * (i+1), 0, gh * self.fontheight, self.colormap_fg[colors.WHITE])
+
+        # Draw windows
+        for c in range(len(layout)-1, -1, -1):
+            y = border 
+
+            for r in range(len(layout[c])):
+                wn, wx, wh = layout[c][r]
+
+                s = self.drawWindow(wn, gww * self.fontwidth - border*2, wh * self.fontheight - border*2)
+                self.screen.blit(s, (c * ww + border, y))
+                y += wh * self.fontheight
+                
+                if r < len(layout[c])-1:
+                    pygame.gfxdraw.hline(self.screen, c * ww, (c+1) * ww,
+                                         y - self.fontheight / 2, self.colormap_fg[colors.WHITE])
+
+        pygame.gfxdraw.hline(self.screen, 0, self.width,
+                             self.height - self.fontheight * 2 - border*2, self.colormap_fg[colors.WHITE])
+
+        if self.modeManager.getMode() == "prompt":
             x = border
-            for c in l:
-                char = self.font.render(c[1], self.antialias, colormap[c[0][0]], (c[0][1] == colors.DEFAULT and self.background or colormap[c[0][1]]))
-                self.screen.blit(char, (x, y))
-                x += self.fontwidth
-            y += self.fontheight
+            y = self.height - self.fontheight * 2 - border
 
-        y -= self.fontheight
-
-        # Prompt line
-        if self.modeManager.getMode() == "normal":
-            curline = self.font.render(self.normalMode.getBuffer(), self.antialias, colormap[colors.YELLOW], self.background)
+            curline = self.font.render(self.promptMode.getText() + self.promptMode.getBuffer(), self.antialias, self.colormap_fg[colors.RED], background)
             self.screen.blit(curline, (x, y))
 
-            cursor = self.font.render("_", self.antialias, colormap[colors.YELLOW])
-            self.screen.blit(cursor, (x + self.normalMode.getCursor()*self.fontwidth, y))
-        elif self.modeManager.getMode() == "prompt":
-            x = border
-            y += 2 * self.fontheight
-
-            curline = self.font.render(self.promptMode.getText() + self.promptMode.getBuffer(), self.antialias, colormap[colors.RED], self.background)
-            self.screen.blit(curline, (x, y))
-
-            cursor = self.font.render("_", self.antialias, colormap[colors.RED])
+            cursor = self.font.render("_", self.antialias, self.colormap_fg[colors.RED])
             self.screen.blit(cursor, (x + (self.promptMode.getCursor() + len(self.promptMode.getText()))*self.fontwidth, y))
 
-
+        status = self.font.render(self.master.session.userStatus, self.antialias, self.colormap_fg[colors.DEFAULT], background)
+        self.screen.blit(status, ((self.width - status.get_width()) / 2, self.height - self.fontheight - border))
         pygame.display.flip()
 
