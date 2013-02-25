acf7881fefdca69d5aa83096d69133672e32706d
Philip Kranz <philip.kranz@googlemail.com>
2013-02-25 11:49:39 +0100

    lua/default: Extended default script.

--- mudblood/lua/default.lua
+++ mudblood/lua/default.lua
@@ -1,10 +1,46 @@
-print("Hallo, willkommen bei Mudblood!")
+nmap("`", function ()
+    prompt("lua: ", function (l)
+        loadstring(l)()
+    end)
+end)
 
-print("Folgende Profile gibt es:")
-print("--------------------------------------------")
-for _,v in ipairs(listProfiles()) do
-    print(v)
+function info(str)
+    print("  " .. str)
 end
 
-print("Lade eins mit: (lua) profile('profilname').load()")
+print(colors.Blue .. [[
+    +---------------------------------------+
+    |  Herzlich willkommen bei mudblood.    |
+    +---------------------------------------+
+]] .. colors.Off)
+
+print([[
+  (Druecke ` oder ! um in den Lua-Modus zu wechseln)
+]])
+
+local profiles = listProfiles()
+
+if profiles == {} then
+    print([[
+Du hast noch keine Profile erstellt. Um ein Profil zu erstellen, lege ein Unterverzeichnis
+in folgendem Verzeichnis an:
+    ]])
+    print(colors.Blue .. path.profileBase() .. colors.Off)
+    print([[
+und erstelle darin eine Datei namens 'profile.lua'.
+    ]])
+else
+    print([[
+Du hast folgende Profile definiert:
+    ]])
+
+    for _,v in ipairs(profiles) do
+        print(v)
+    end
+
+    print([[
+
+Du kannst ein Profil laden mit: [lua] ]] .. colors.Blue .. [[profile("profilname").load()]] .. colors.Off .. [[
+    ]])
+end
 
