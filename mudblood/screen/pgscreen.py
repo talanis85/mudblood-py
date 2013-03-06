5a252d41e6cbbf4f346001b901cb515a89df5520
Philip Kranz <philip.kranz@googlemail.com>
2013-03-06 13:02:40 +0100

    screen/pgscreen: Improved drawing efficiency.
    Only changed windows will be redrawn.

--- mudblood/screen/pgscreen.py
+++ mudblood/screen/pgscreen.py
@@ -65,33 +65,22 @@ class Lua_Screen(lua.LuaExposedObject):
 
     def windowVisible(self, name, value=None):
         if value is None:
-            if name == 'main':
-                return True
-            return (name in self._screen.windows)
+            return self._screen.windowManager.hasWindow()
         else:
-            if name == 'main':
-                return
-
             if value == False and name in self._screen.windows:
-                self._screen.windows.remove(name)
+                self._screen.windowManager.removeWindow(name)
 
             if value == True:
-                if name not in self._screen.windows:
-                    self._screen.windows.append(name)
-                if name not in self._screen.window_sizes:
-                    self._screen.window_sizes[name] = 10
+                self._screen.windowManager.addWindow(name)
 
     def windowSize(self, name, value=None):
         if value == None:
-            if name in self._screen.window_sizes:
-                return self._screen.window_sizes[name]
-            else:
-                return None
+            return self._screen.windowManager.getWindow(name).size
         else:
-            self._screen.window_sizes[name] = value
+            self._screen.windowManager.getWindow(name).size = value
 
     def setColumns(self, num):
-        self._screen.columns = num
+        self._screen.windowManager.columns = num
     
     def configFgColor(self, colornum, rgb):
         self._screen.colormap_fg[colornum] = (rgb[1], rgb[2], rgb[3])
@@ -102,6 +91,199 @@ class Lua_Screen(lua.LuaExposedObject):
     def scroll(self, value, name='main'):
         self._screen.moveScroll(name, value)
 
+class WindowManager(object):
+    def __init__(self, screen):
+        self.screen = screen
+        self.layout = []
+        self.columns = 1
+        self.windows = []
+        self.windowObjects = {}
+
+    def addWindow(self, name):
+        if name not in self.windows:
+            self.windows.append(name)
+
+    def removeWindow(self, name):
+        if name in self.windows:
+            self.windows.remove(name)
+
+    def hasWindow(self, name):
+        return name == 'main' or (name in self.windows)
+
+    def arrange(self, gh):
+        gx = 0
+        cc = self.columns - 1
+        cr = 0
+
+        self.layout = []
+        for c in range(self.columns):
+            self.layout.append([])
+
+        for w in self.windows + ['main']:
+            wh = 0
+            if w == 'main':
+                if cc > 0:
+                    if cr > 0:
+                        self.layout[cc][cr-1] = (self.layout[cc][cr-1][0], self.layout[cc][cr-1][1], gh - self.layout[cc][cr-1][1])
+                    cc = 0
+                    cr = 0
+                    gx = 0
+                wh = gh - gx
+            else:
+                wh = self.getWindow(w).size
+
+            if gx + wh > gh and cc > 0:
+                if cr > 0:
+                    self.layout[cc][cr-1] = (self.layout[cc][cr-1][0], self.layout[cc][cr-1][1], gh - self.layout[cc][cr-1][1])
+                cc -= 1
+                cr = 0
+                gx = 0
+
+            if gx + wh > gh:
+                wh = gh - gx
+
+            self.layout[cc].append((w, gx, wh))
+            cr += 1
+            gx += wh
+
+    def __iter__(self):
+        return iter(self.layout)
+    
+    def getWindow(self, name):
+        if name not in self.windowObjects:
+            self.windowObjects[name] = Window.create(self.screen, name)
+
+        return self.windowObjects[name]
+
+class Window(object):
+    @classmethod
+    def create(cls, screen, name):
+        if name == 'map':
+            return MapWindow(screen, name)
+        elif name == 'main':
+            return MainWindow(screen, name)
+        else:
+            return LinebufferWindow(screen, name)
+
+    def __init__(self, screen, name):
+        self.screen = screen
+        self.name = name
+        self.size = 10
+        self.refw = 0
+        self.refh = 0
+
+    def setSize(self, size):
+        self.size = size
+
+class LinebufferWindow(Window):
+    def __init__(self, screen, name):
+        super(LinebufferWindow, self).__init__(screen, name)
+
+        self.ref = None
+        self.fixh = 5
+        self.surface = None
+        self.scroll = 0
+
+    def draw(self, w, h):
+        if self.name not in self.screen.master.session.linebuffers:
+            if self.surface is None:
+                self.surface = pygame.Surface((w, h))
+                self.surface.fill(self.screen.colormap_bg[colors.DEFAULT])
+            return self.surface
+            
+        lb = self.screen.master.session.linebuffers[self.name]
+
+        if lb.head() is not self.ref or self.refw != w or self.refh != h:
+            self.surface = None
+            self.ref = lb.head()
+            self.refw = w
+            self.refh = h
+
+        if self.surface is not None:
+            return self.surface
+
+        background = self.screen.colormap_bg[colors.DEFAULT]
+
+        surf = pygame.Surface((w, h))
+        surf.fill(background)
+
+        y = 0
+        x = 0
+
+        gw = w / self.screen.fontwidth
+        gh = h / self.screen.fontheight
+
+        lines = None
+        fixlines = None
+
+        if self.scroll > 0:
+            pygame.gfxdraw.hline(surf, 0, w, h - self.fixh * self.screen.fontheight - self.screen.fontheight / 2,
+                                 self.screen.colormap_fg[colors.WHITE])
+
+            lines = lb.render(gw, self.scroll, gh - self.fixh - 1) \
+                  + [[]] + lb.render(gw, 0, self.fixh)
+        else:
+            lines = lb.render(gw, self.scroll, gh)
+
+        if len(lines) < gh:
+            y += (gh - len(lines)) * self.screen.fontheight
+
+        for l in lines:
+            x = 0
+            for ch in l:
+                char = self.screen.font.render(ch[1], self.screen.antialias,
+                                               self.screen.colormap_fg[ch[0][0]],
+                                               (ch[0][1] == colors.DEFAULT and background or self.colormap_bg[ch[0][1]]))
+                surf.blit(char, (x, y))
+                x += self.screen.fontwidth
+            y += self.screen.fontheight
+
+        self.surface = surf
+
+        return self.surface
+
+class MainWindow(LinebufferWindow):
+    def __init__(self, screen, name):
+        super(MainWindow, self).__init__(screen, name)
+
+        self.promptRef = ""
+        self.cursorRef = 0
+        self.surface2 = None
+
+    def draw(self, w, h):
+        surf = super(MainWindow, self).draw(w, h - self.screen.fontheight)
+
+        line = self.screen.master.session.getPromptLine() + colors.AString(self.screen.normalMode.getBuffer()).fg(colors.YELLOW)
+
+        if self.promptRef != line or self.cursorRef != self.screen.normalMode.getCursor():
+            self.surface2 = None
+            self.promptRef = line 
+            self.cursorRef = self.screen.normalMode.getCursor()
+
+        if self.surface2 is None:
+            self.surface2 = pygame.Surface((w, self.screen.fontheight))
+
+            x = 0
+            for ch in line:
+                char = self.screen.font.render(ch[1], self.screen.antialias,
+                                               self.screen.colormap_fg[ch[0][0]],
+                                               self.screen.colormap_bg[ch[0][1]])
+                self.surface2.blit(char, (x, 0))
+                x += self.screen.fontwidth
+
+            cursor = self.screen.font.render("_", self.screen.antialias, self.screen.colormap_fg[colors.YELLOW])
+            self.surface2.blit(cursor, ((len(self.screen.master.session.getPromptLine()) + self.screen.normalMode.getCursor())*self.screen.fontwidth, 0))
+
+        combined = pygame.Surface((w, h))
+        combined.blit(self.surface, (0, 0))
+        combined.blit(self.surface2, (0, h - self.screen.fontheight))
+
+        return combined
+
+class MapWindow(Window):
+    def draw(self, w, h):
+        return pygame.Surface((w, h))
+
 class PygameScreen(modalscreen.ModalScreen):
     def __init__(self, master):
         super(PygameScreen, self).__init__(master)
@@ -109,9 +291,7 @@ class PygameScreen(modalscreen.ModalScreen):
         self.width = 1000
         self.height = 600
 
-        self.windows = []
-        self.window_sizes = {}
-        self.columns = 1
+        self.windowManager = WindowManager(self)
 
         self.colormap_fg = {
                 colors.BLACK:           (0, 0, 0),
@@ -205,122 +385,26 @@ class PygameScreen(modalscreen.ModalScreen):
 
             self.doUpdate()
 
-    def drawWindow(self, wn, w, h, fixh=5):
-        surf = pygame.Surface((w, h))
-        surf.fill(self.colormap_bg[colors.DEFAULT])
-
-        background = self.colormap_bg[colors.DEFAULT]
-
-        y = 0
-        x = 0
-
-        gw = w / self.fontwidth
-        gh = h / self.fontheight
-
-        if wn == 'map':
-            # TODO
-            pass
-        else:
-            if wn in self.master.session.linebuffers:
-                lines = None
-                fixlines = None
-                scroll = self.getScroll(wn)
-                lb = self.master.session.linebuffers[wn]
-
-                if scroll > 0:
-                    pygame.gfxdraw.hline(surf, 0, w, h - fixh * self.fontheight - self.fontheight / 2,
-                                         self.colormap_fg[colors.WHITE])
-
-                    if wn == 'main':
-                        lines = lb.render(gw, scroll, gh - fixh - 1) \
-                              + [[]] + lb.render(gw, 0, fixh - 1) + [self.master.session.getPromptLine()]
-                    else:
-                        lines = lb.render(gw, scroll, gh - fixh - 1) \
-                              + [[]] + lb.render(gw, 0, fixh)
-                else:
-                    if wn == 'main':
-                        lines = lb.render(gw, scroll, gh - 1) \
-                              + [self.master.session.getPromptLine()]
-                    else:
-                        lines = lb.render(gw, scroll, gh)
-
-                if len(lines) < gh:
-                    y += (gh - len(lines)) * self.fontheight
-
-                for l in lines:
-                    x = 0
-                    for ch in l:
-                        char = self.font.render(ch[1], self.antialias,
-                                                self.colormap_fg[ch[0][0]],
-                                                (ch[0][1] == colors.DEFAULT and background or self.colormap_bg[ch[0][1]]))
-                        surf.blit(char, (x, y))
-                        x += self.fontwidth
-                    y += self.fontheight
-
-                if wn == 'main':
-                    y -= self.fontheight
-
-                    curline = self.font.render(self.normalMode.getBuffer(), self.antialias,
-                                               self.colormap_fg[colors.YELLOW], background)
-                    surf.blit(curline, (x, y))
-
-                    cursor = self.font.render("_", self.antialias, self.colormap_fg[colors.YELLOW])
-                    surf.blit(cursor, (x + self.normalMode.getCursor()*self.fontwidth, y))
-
-        return surf
-
     def doUpdate(self):
         background = self.colormap_bg[colors.DEFAULT]
         self.screen.fill(background)
 
-        border = 10
+        border = self.fontheight / 2
 
         status = [x for x in self.master.session.userStatus.splitLines()]
 
         gw = self.width / self.fontwidth
         gh = self.height / self.fontheight - 2 - len(status)
 
-        gww = gw / self.columns
-        ww = self.width / self.columns
-
-        # Arrange windows
-        gx = 0
-        cc = self.columns - 1
-        cr = 0
-
-        layout = []
-        for c in range(self.columns):
-            layout.append([])
-
-        for w in self.windows + ['main']:
-            if w == 'main':
-                if cc > 0:
-                    if cr > 0:
-                        layout[cc][cr-1] = (layout[cc][cr-1][0], layout[cc][cr-1][1], gh - layout[cc][cr-1][1])
-                    cc = 0
-                    cr = 0
-                    gx = 0
-                wh = gh - gx
-            else:
-                wh = self.window_sizes[w]
-
-            if gx + wh > gh and cc > 0:
-                if cr > 0:
-                    layout[cc][cr-1] = (layout[cc][cr-1][0], layout[cc][cr-1][1], gh - layout[cc][cr-1][1])
-                cc -= 1
-                cr = 0
-                gx = 0
-
-            if gx + wh > gh:
-                wh = gh - gx
-
-            layout[cc].append((w, gx, wh))
-            cr += 1
-            gx += wh
+        gww = gw / self.windowManager.columns
+        ww = self.width / self.windowManager.columns
 
         # Draw column separators
-        for i in range(self.columns-1):
+        for i in range(self.windowManager.columns-1):
             pygame.gfxdraw.vline(self.screen, ww * (i+1), 0, gh * self.fontheight, self.colormap_fg[colors.WHITE])
+
+        self.windowManager.arrange(gh)
+        layout = self.windowManager.layout
 
         # Draw windows
         for c in range(len(layout)-1, -1, -1):
@@ -329,7 +413,7 @@ class PygameScreen(modalscreen.ModalScreen):
             for r in range(len(layout[c])):
                 wn, wx, wh = layout[c][r]
 
-                s = self.drawWindow(wn, gww * self.fontwidth - border*2, wh * self.fontheight - border*2)
+                s = self.windowManager.getWindow(wn).draw(gww * self.fontwidth - border*2, wh * self.fontheight - border*2)
                 self.screen.blit(s, (c * ww + border, y))
                 y += wh * self.fontheight
                 
